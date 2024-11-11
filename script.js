bf74ed14af42fd5454d3d2a14126bbc1a75d6b35
Vinslol1 <167725416+Vinslol1@users.noreply.github.com>
2024-11-11 14:37:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-// search-box open close js code
-let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+  // search-box open close js code
+  let navbar = document.querySelector(".navbar");
+  let searchBox = document.querySelector(".search-box .bx-search");
+  // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-searchBox.addEventListener("click", ()=>{
-  navbar.classList.toggle("showInput");
-  if(navbar.classList.contains("showInput")){
-    searchBox.classList.replace("bx-search" ,"bx-x");
-  }else {
-    searchBox.classList.replace("bx-x" ,"bx-search");
-  }
-});
+  searchBox.addEventListener("click", ()=>{
+    navbar.classList.toggle("showInput");
+    if(navbar.classList.contains("showInput")){
+      searchBox.classList.replace("bx-search" ,"bx-x");
+    }else {
+      searchBox.classList.replace("bx-x" ,"bx-search");
+    }
+  });
 
-// sidebar open close js code
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
-}
+  document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelector(".nav-links");
+    const menuOpenBtn = document.querySelector(".navbar .bx-menu");
+    const menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
+
+    // Open sidebar
+    menuOpenBtn.addEventListener("click", () => {
+      navLinks.style.left = "0"; // Sidebar slides in
+    });
+
+    // Close sidebar
+    menuCloseBtn.addEventListener("click", () => {
+      navLinks.style.left = "-100%"; // Sidebar slides out
+    });
+  });
 
 
