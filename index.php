93852a293fb2768b13a421071f82fca18113acec
SILVERGOLDZ <thunderraiden655@gmail.com>
2024-11-30 14:18:08 +0700

    conn supa

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-    $connection = pg_connect("host = localhost dbname = MAHASISWA user = postgres password=@13579@08642@@");
-    if (!$connection) {
-        echo "EROR";
-        exit;
-    }
-
-    $result = pg_query($connection, "SELECT * from MAHASISWA");
-    if (!$result){
-        echo "ERORRRRRRRRRRRRRR!";
-        exit;
-    }
-
-    $test = pg_fetch_row($result);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
